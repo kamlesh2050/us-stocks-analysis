9219d033e9574d543a4dda353037b50ee0178f79
kamlesh2050 <105483733+kamlesh2050@users.noreply.github.com>
2025-11-30 12:17:03 -0500

    Push to create StocksAnalysisApplication.java

---
+++ src/main/java/com/stocks/StocksAnalysisApplication.java
@@ -0,0 +1,11 @@
+package com.stocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksAnalysisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StocksAnalysisApplication.class, args);
+    }
+}
